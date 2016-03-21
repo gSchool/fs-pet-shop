9daf1d1ed9fb2c2db3b11b8568ddf3b2a1ba38b1
Vincent Markham <vincent.markham@gmail.com>
2016-03-21 14:12:44 -0700

    add read command

--- pets.js
+++ pets.js
@@ -2,12 +2,24 @@
 
 var fs = require('fs');
 var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
 
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
-var cmd = process[2];
+var cmd = process.argv[2];
 
-if (!cmd) {
+if (cmd === 'read') {
+  fs.readFile(petsPath, 'utf8', function(err, data){
+    if (err) {
+      throw err;
+    }
+
+    var pets = JSON.parse(data);
+
+    console.log(pets);
+  });
+}
+else {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
   process.exit(1);
 }
