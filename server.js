fcca7b7d87e2f6d2408443bb6cf30718c5776375
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-09-16 13:54:06 -0500

    express server done

--- server.js
+++ server.js
@@ -1,18 +1,44 @@
-var http = require('http');
-var routes = require('./routes.js');
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
 
 
-function handleRequest(req, res) {
-    if (routes[req.url]) {
-        routes[req.url](req, res);
-    } else {
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Route 404, no such route.');
-    }
-}
 
-var server = http.createServer(handleRequest);
+app.get('/pets', function(req, res) {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
 
-server.listen(8080, 'localhost', function() {
-    console.log('Listening on port 8080');
-})
+        var pets = JSON.parse(petsJSON);
+
+        res.send(pets);
+    });
+});
+
+app.get('/pets/0', function(req, res) {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+
+        var pets = JSON.parse(petsJSON);
+
+        res.send(pets[0]);
+    });
+});
+
+app.get('/pets/1', function(req, res) {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+
+        var pets = JSON.parse(petsJSON);
+
+        res.send(pets[1]);
+    });
+});
+
+
+app.use(function(req, res) {
+    res.send('404, not found');
+});
+
+app.listen(port, function() {
+    console.log('Listening on port', port);
+});
