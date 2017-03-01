3f0bb5fa52e8c48d0e251883e09bc2cfb2029de6
Bill Boughton <boughton.william@gmail.com>
2017-02-28 20:36:35 -0700

    add the exress server and changed the http server

--- httpServer.js
+++ httpServer.js
@@ -1,27 +1,32 @@
-const express = require('express')
-const app = express()
+
+const server = require('http')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 var allPets = require('./pets.json')
 
 var PORT = 8000
 
-app.use(bodyParser.json())
+// server.use(bodyParser.json())
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log('Server listening on port ' + PORT)
 });
 
-app.get('/pets', (req, res) => {
+server.get('/pets', (req, res) => {
   res.status(200).json(allPets)
 })
 
-app.get('/pets/:id', (req, res) => {
+server.get('/pets/:id', (req, res) => {
   var id = req.params.id
   if (req.params.id < 0 || req.params.id >= allPets.length) {
-    res.status(404).send("Not Found")
+    res.statuscode = 404
+    res.setHeader('Content-Type', /text\/plain/)
+    res.send('Not Found')
+    // Content-Type: text/plain;
+    // res.send(404, 'Content-Type', /text\/plain/)
   } else {
-  console.log(allPets[id]);
   res.status(200).json(allPets[id])
   }
 })
+
+module.exports = server
