46fe0b86cfab00b90e5617ab27aaebfd23e84b44
Paul Torres <torrepaul@gmail.com>
2017-03-01 19:54:15 -0700

    halfway through tests for pets basic

--- pets.js
+++ pets.js
@@ -1,31 +1,66 @@
 const fs = require('fs');
+const path = require('path');
 
 // var buf = fs.readFileSync('/path/to/file')
 
-// console.log(process.argv);
-var myArgs = process.argv.slice(2);
+// console.log(process.argv); command is better
+
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var command = process.argv[2];
+var index = process.argv[3];
 
 var petsData = 'pets.json';
-///read it , then JSON.parse JSON.stringify
 
-var ourPetData =fs.readFileSync(petsData);
+if (command === 'read') {
+  fs.readFile(petsData, 'utf8', (err, data) => {
 
-var scrubbedData = JSON.parse(ourPetData);
+    if (err) {
+      throw err
+    }
 
-
-fs.readFile(PetsData, 'utf8', function (err, contents) {
-  if ( myArgs[0] === 'read') {
-    console.log(contents);
-    console.log(scrubbedData);
-  }
-  else if ( myArgs[0] === 'write' ) {
-    console.log('something happened');
-    console.log(myArgs);
-  }
-  else if (err) {
-    return console.log('Command produced a zero status code');
-  }
+    var scrubbedData = JSON.parse(data);
+    if (index) {
+        if (index <= scrubbedData.length-1 && index > -1) {
+          console.log(scrubbedData[index]);
+        }
+        else {
+          console.error(`Usage: ${node} ${file} ${command} INDEX`);
+          process.exit(5);
+        }
+      }
+    else {
+      console.log(scrubbedData);
+    }
+  })
 }
+
+else if (command === 'create') {
+  fs.readFile(petsData, 'utf8', (err, data) => {
+   
+  })
+}
+
+
+else {
+  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+  process.exit(5);
+
+}
+
+// fs.readFile(PetsData, 'utf8', function (err, contents) {
+//   if ( myArgs[0] === 'read') {
+//     console.log(contents);
+//     console.log(scrubbedData);
+//   }
+//   else if ( myArgs[0] === 'write' ) {
+//     console.log('something happened');
+//     console.log(myArgs);
+//   }
+//   else if (err) {
+//     return console.log('Command produced a zero status code');
+//   }
+// });
 
 
 //process.exit
