a477f7f1200a1d009c311303dc411620c5e4cf80
Paul Torres <torrepaul@gmail.com>
2017-03-03 13:17:05 -0700

    part of the way through

---
+++ expressServer.js
@@ -0,0 +1,58 @@
+'use strict';
+//moved app.use below the .get, b/c otherwise the get request returned a 404- I take it this is one of those top to bottom things?
+
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
+
+const petsData = 'pets.json';
+
+
+const PORT = process.env.PORT || 3005;
+app.listen( PORT, () => {
+  console.log('listening on port', PORT)
+})
+
+
+app.get('/pets', (req, res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+  var pets = JSON.parse(petsJSON);
+  res.send(pets);
+  })
+})
+
+app.get('/pets/:id', (req,res) => {
+  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var pets = JSON.parse(petsJSON);
+// have an issue with res.send inside of this function....a res.send works up above, but here it causes an error wtf!
+    // res.set('Content-Type','text/plain');
+    res.sendStatus(200).send(id);
+    // res.sendStatus(200);
+  })
+})
+
+app.use((req, res) => {
+  res.sendStatus(404);
+  // sends error, but no string attached, cant send multiple replies ergo line below commented out!
+  // res.send('error');
+
+});
+
+
+
+module.exports = app;
