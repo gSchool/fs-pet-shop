9789fb2b926d3446dcbe94a2bea3fbc721b6fa5d
lanefaison2 <faisonusmc@gmail.com>
2017-03-02 17:55:09 -0700

    POST/DELETE tests not working

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -3,12 +3,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const fs = require('fs')
-// const morgan = require('morgan')
+const morgan = require('morgan')
 const PORT = process.env.PORT || 8000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-// app.use(morgan('tiny'))
+app.use(morgan('default'))
 
 // LISTEN
 app.listen(PORT, function () {
@@ -18,87 +18,99 @@ app.listen(PORT, function () {
 // GET PETS
 app.get('/pets', function (req, res) {
   res.status(200).json(pets)
-  console.log(req.url)
+})
+
+// GET PETS BY ID
+app.get('/pets/:id', function (req, res) {
+  const index = req.params.id
+  if (index >= 0 && index < pets.length) {
+    res.status(200).json(pets[index])
+  }
+  else {
+    res.setHeader('Content-Type','text/plain')
+    res.status(404).send('Not Found')
+  }
 })
 
 // POST PETS
 app.post('/pets/', function (req, res) {
 
-  newPet = {
-    age: parseInt(req.headers.age),
-    kind: req.headers.kind,
-    name: req.headers.name
-  }
-
-  pets.push(newPet)
-  var petArray = JSON.stringify(pets)
-  fs.writeFileSync('./pets.json',petArray)
-  res.end()
-})
-
-app.patch('/pets/:id', function (req, res) {
-  const petId = parseInt(req.params.id)
-  $.get('pets[petId]')
-
   var petAge = parseInt(req.headers.age)
   var petKind = req.headers.kind
   var petName = req.headers.name
 
-  if (petAge.isNaN() || !petKind || !petName) {
-    res.status(404).send('Missing Parts')
-  } else {
+  if (!isNaN(petAge) && !!petKind && !!petName) {
+
     newPet = {
-      age: parseInt(req.headers.age),
-      kind: req.headers.kind,
-      name: req.headers.name
+      age: petAge,
+      kind: petKind,
+      name: petName
     }
-    .assign(newPet)
-    .write()
-    .then(updatedPet => {
-      res.send(updatedPet);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-    // var petArray = JSON.stringify(pets)
-    // fs.writeFileSync('./pets.json',petArray)
-    // res.end()
+
+    pets.push(newPet)
+    fs.writeFileSync('./pets.json', JSON.stringify(pets))
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200).send(pets)
+  }
+  else {
+    res.setHeader("Content-Type", "text/plain")
+    res.status(400).send('Bad Request')
   }
 })
 
+// PATCH PETS
+app.patch('/pets/:id', function (req, res) {
+  const index = parseInt(req.params.id)
 
+  if (index >=0 && index < pets.length) {
+
+    if (isNaN(req.headers.age)) {
+      var petAge = pets[index].age
+    } else {
+      var petAge = parseInt(req.headers.age)
+    }
+    if (!req.headers.name) {
+      var petName = pets[index].name
+    } else {
+      var petName = req.headers.name
+    }
+    if (!req.headers.kind) {
+      var petKind = pets[index].kind
+    } else {
+      var petKind = req.headers.kind
+    }
+
+    var updatedPet = {
+      age: petAge,
+      kind: petKind,
+      name: petName
+    }
+
+    pets[index] = updatedPet
+
+    fs.writeFileSync('./pets.json', JSON.stringify(pets))
+    res.setHeader("Content-Type", "application/json")
+    res.end()
+  }
+
+  else {
+    res.status(400).send('Bad Request')
+  }
+})
+
+// DELETE PET
+app.delete('/pets/:id', function (req, res) {
+  const index = parseInt(req.params.id)
+
+  if (index >= 0 && index < pets.length) {
+    pets.splice(index, 1)
+
+    fs.writeFileSync('./pets.json', JSON.stringify(pets))
+    res.status(200).json(pets)
+  }
+  else {
+    res.status(404).send('Not Found')
+  }
+})
 
 module.exports = app
-
-// app.use('/pets', function (req, res) {
-//   res.send('Our first express program')
-// })
-
-// morgan.token('name', function (req, res) {
-//   return req.headers['content-type']
-// })
-//
-//
-// app.get('/pets/:id', function (req, res) {
-//   const index = req.url.substring(req.url.lastIndexOf('/')+1, req.url.length)
-//   if (index >= 0 && index < req.url.length) {
-//     res.status(200).json(pets[index])
-//   } else {
-//     res.writeHead(404, {"Content-Type": "text/plain"})
-//     res.status(404).send('Not Found')
-//   }
-// })
-//
-// morgan.token('id', function getId(req) {
-//   console.log(req.id);
-// })
-
-
-
-// var router = express.Router()
-// router.get('/pets', function (req, res) {
-//   res.json({ message: 'horray!'})
-//   res.status(200).json(pets)
-// })
-
-// app.use('/api', router)
