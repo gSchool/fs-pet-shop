447b53eea39dbea9766af909244e2984e0272800
Brian James <brianpja@gmail.com>
2017-07-17 15:11:06 -0700

    add create command and passing tests

--- pets.js
+++ pets.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const petsPath = path.join(__dirname, 'pets.json');
 
 const node = path.basename(process.argv[0]);
 const file = path.basename(process.argv[1]);
@@ -9,5 +10,70 @@ const cmd = process.argv[2];
 
 
 
-console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
-process.exit(1);
+
+if (cmd === 'read') {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+
+    const pets = JSON.parse(data);
+    const index = process.argv[3];
+
+    if (index) {
+
+      if (0 > index || index >= pets.length) {
+        console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+        process.exit(1);
+      }
+
+      console.log(pets[index]);
+
+    }
+    else {
+      console.log(pets);
+    }
+  })
+}
+
+else if (cmd === 'create') {
+    fs.readFile(petsPath, 'utf8', function(readErr, data) {
+      if (readErr) {
+        throw readErr;
+      }
+
+      const pets = JSON.parse(data);
+      const age = parseInt(process.argv[3]);
+      const kind = process.argv[4];
+      const name = process.argv[5];
+
+      if (!name) {
+        console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+        process.exit(1);
+      }
+      else {
+        const pet = {
+          'age': age,
+          'kind': kind,
+          'name': name
+        };
+        pets.push(pet);
+
+        const petsJSON = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, petsJSON, function(writeErr) {
+          if (writeErr) {
+            throw writeErr;
+          }
+
+          console.log(pet);
+      });
+
+    };
+  });
+}
+
+else {
+  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+  process.exit(1);
+}
