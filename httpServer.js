b5150173d55918de6865fa05f9c679c411175f61
donkeypagoda <christopherallenstewart@gmail.com>
2017-07-18 15:32:33 -0700

    1st test

--- httpServer.js
+++ httpServer.js
@@ -6,17 +6,26 @@ const path = require("path");
 const petsPath = path.join(__dirname, "pets.json");
 
 const server = http.createServer(function (req, res){
-  if (req.method === get){
-    if (req.url === '/\/pets.*/'){
+  if (req.method === "GET"){
+    if (req.url === "/pets"){
+      fs.readFile(petsPath, 'utf8', function (err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
 
-      console.log(req.url);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Internal Server Error');
+
+        return;
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petsJSON);
+    });
+
     }
+
   }
-
-
-
-
-
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
@@ -29,6 +38,5 @@ const port = 8000;
 server.listen(port, function(){
   console.log(`Listening on port ${port}`);
 });
-
 
 module.exports = server;
