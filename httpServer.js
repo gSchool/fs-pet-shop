5ebf03840765cfa17c871a60ac224d1c0c4ebed3
donkeypagoda <christopherallenstewart@gmail.com>
2017-07-18 15:03:52 -0700

    server up

--- httpServer.js
+++ httpServer.js
@@ -3,11 +3,32 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 
-const server = http.createServer()
+const petsPath = path.join(__dirname, "pets.json");
+
+const server = http.createServer(function (req, res){
+  if (req.method === get){
+    if (req.url === '/\/pets.*/'){
+
+      console.log(req.url);
+    }
+  }
 
 
 
 
+
+  else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+const port = 8000;
+
+server.listen(port, function(){
+  console.log(`Listening on port ${port}`);
+});
 
 
 module.exports = server;
