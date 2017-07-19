cf7368c79ff6fe2510a5c5a9b7ab6004bb8c51c5
donkeypagoda <christopherallenstewart@gmail.com>
2017-07-19 14:38:31 -0700

    got the basics of the express server built

---
+++ expressServer.js
@@ -0,0 +1,53 @@
+'use strict';
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+app.disable("x-powered-by");
+
+const petsPath = path.join(__dirname, "pets.json");
+
+app.get("/pets", (req, res) => {
+  fs.readFile(petsPath, "utf8", (err, petsJSON) => {
+    if(err){
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    const pets = JSON.parse(petsJSON);
+    res.send(pets);
+  });
+});
+
+app.get("/pets:id", (req, res) => {
+  fs.readFile(petsPath, "utf8", (err, petsJSON) =>{
+    if(err){
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    const id = Number.parseInt(req.params.id);
+    const pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)){
+      return res.sendStatus(404);
+    }
+    res.set("Content-Type", "text/plain");
+    res.send(pets[id]);
+  });
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+
+
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
