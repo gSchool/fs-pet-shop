fe3107da7d8cb484334a370ee9a0eac274eb829c
Kashi Rana Halma <kashi.halma@gmail.com>
2017-09-06 15:45:15 -0600

    asdf

--- pets.js
+++ pets.js
@@ -1,120 +1,79 @@
 'use strict'
-
 var counter=0;
-var juno=process.argv;
+var commandArr=process.argv;
+var fs= require('fs');
 var regy=/read|create|update|destroy/;
-
-/*
-if(juno.length===2)
-{
-  console.error("Usage: node pets.js create AGE KIND NAME")
-  process.exit(1);
-}
-*/
-
-
-
-for(var moon=0; moon<juno.length; moon++)
-{
-  if(regy.test(juno[moon])===true)
-  {
-    counter++;
+var numberReg=/[0-9]/;
+for(var i=0; i<commandArr.length; i++){
+  if(regy.test(commandArr[i])===true){
+    counter++
   }
 }
-if(counter===0)
-{
+if(counter===0){
   console.error("Usage: node pets.js [read | create | update | destroy]")
   process.exit(1);
 }
-
-var fs = require("fs");
-var pink= /-|[0-9]/
-for(var i=0 ;i<juno.length; i++)
-{
-  if(juno[i]==="read")
-  {
-
-    if(pink.test(Number(juno[i+1]))===true)
-    {
-      if(Number(juno[i+1])<0||Number(juno[i+1])>=juno.length)
-      {
-
-      process.exit(1);
-
-      }
-      var index=juno[i+1];
-
+for(var j=0; j<commandArr.length; j++){
+  if(commandArr[j]==='read'&&commandArr.length===3){
     fs.readFile("pets.json", function(err, data){
-      if(err)
-      {
-        console.log("you are wrong");
+      if(err){
+        console.error("Usage: node pets.js [read | create | update | destroy]")
+        process.exit(1);
       }
-      console.log(JSON.parse(data)[index]);
-
-
-    });
+      console.log(JSON.parse(data))
+    })
   }
+}
 
-    else
-      {
-        fs.readFile("pets.json", function(err, data){
-          if(err)
-          {
-            //console.log("you are wrong");
-          }
-          console.log(JSON.parse(data));
-
-        });
+for(var k=0; k<commandArr.length; k++){
+  if(commandArr[k]==='read'&&numberReg.test(Number(commandArr[k+1]))===true){
+    fs.readFile("pets.json", function(err, data){
+      if(err){
+        console.error("Usage: node pets.js [read | create | update | destroy]")
+        process.exit(1);
       }
-
+      console.log(JSON.parse(data)[Number(commandArr[commandArr.length-1])]);
+    })
   }
-  else if(juno[i]==="create"&&juno.length<6)
-  {
+}
+for(var x=0; x<commandArr.length; x++){
+  if(commandArr[x]==='create'&&commandArr.length<6){
     console.error("Usage: node pets.js create AGE KIND NAME")
     process.exit(1);
   }
-  else if(juno[i]==="create"&&juno.length===6&&isNaN(juno[i+1])===false)
-  {
-        var animalObj= new Object();
-        animalObj.age=Number(juno[i+1]);
-        animalObj.kind=juno[i+2];
-        animalObj.name=juno[i+3];
-        //console.log(JSON.stringify(animalObj));
-        var JSONAnimalObj = JSON.stringify(animalObj);
-        console.log(animalObj);
+}
+for(var z=0; z<commandArr.length; z++){
+  if(commandArr[z]==='create'&&commandArr.length===6){
+    var petObj= new Object();
+    petObj.age= Number(commandArr[z+1]);
+    petObj.kind=commandArr[z+2];
+    petObj.name=commandArr[z+3];
+    console.log(petObj)
 
-        fs.readFile("pets.json", "utf8", function(err, data){
-          if(err)
-          {
-            console.error("Usage: node pets.js create AGE KIND NAME")
-            process.exit(1);
-          }
-          //console.log(data)
-          //var javaData=JSON.parse(data);
-          //javaData.push(animalObj);
-          //console.log(javaData)
-          var animalArr=[];
-          animalArr.push(animalObj);
-          var JSONAnimalArr=JSON.stringify(animalArr);
-          console.log(JSONAnimalArr)
-          //var JSONd=JSON.stringify(javaData)
-          //console.log(JSONd)
-          //fs.writeFile("pets.json", JSONd)
-          fs.writeFile("pets.json", JSONAnimalArr);
-
-
-
-        });
-
-
-/*
-
-      fs.writeFile("pets.json", JSONAnimalObj, function(err){
-        if(err)
-        {
-          console.log("something went wrong");
+    fs.readFile("pets.json", function(err, data){
+      if (err) {
+        assert.fail('Command produced a nonzero status code',
+          'Command should produce a zero status code');
+      }
+      var newPetsArr=[];
+      var petsArr=JSON.parse(data);
+      petsArr.push(petObj);
+      newPetsArr.push(petObj);
+      // newPetsArr.push("");
+      // console.log(newPetsArr)
+      //console.log(petObj)
+      fs.writeFile("pets.json", petObj, function(err){
+        if(err){
+          console.error("Usage: node pets.js create AGE KIND NAME")
+          process.exit(1);
         }
-      });
-*/
+
+      })
+      // console.log("")
+      //console.log(petsArr)
+    })
+
+    // console.log('{ age: 3, kind: \'parakeet\', name: \'Cornflake\' }')
+
   }
 }
