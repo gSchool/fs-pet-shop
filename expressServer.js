6367772a8dd28d3e9efee7f1c35dbcd98727919c
Jason Amador <jason.amador@gmail.com>
2017-11-28 14:27:33 -0600

    first test passing

--- expressServer.js
+++ expressServer.js
@@ -22,10 +22,16 @@ app.get('/pets/:id', (req, res) => {
     if (err) {
       res.sendStatus(500);
     } else {
-      try {
-        res.send(JSON.parse(data)[req.params.id]);
-      } catch (e){
-        res.sendStatus(500);
+      let id = Number.parseInt(req.params.id);
+      let pets = JSON.parse(data);
+      if (id >= 0 && id < pets.length) {
+        try {
+          res.send(pets[id]);
+        } catch (e){
+          res.sendStatus(500);
+        }
+      } else {
+        res.sendStatus(404);
       }
     }
   })
@@ -38,3 +44,5 @@ app.use((req, res) => {
 app.listen(port, () => {
   console.log("listening on port " + port);
 });
+
+module.exports = app;
