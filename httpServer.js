737ebadb4bf3d58473ac4b866d81a88ddcc65e4a
Jason Amador <jason.amador@gmail.com>
2017-11-28 07:49:34 -0600

    seems to work, but failing tests. wtf

--- httpServer.js
+++ httpServer.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-      read(req, res, req.url.split('/')[2]);
+  const route = req.url.split('/');
+  route.splice(0, 1);
+  if (route[0] === 'pets') {
+    if (req.method === 'GET') {
+      read(req, res, route[1]);
+    }
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not found');
   }
 });
 
@@ -10,32 +17,35 @@ let readCounts = 0;
 
 function read(req, res, index) {
   readCounts++;
+  let err = false;
   require('fs').readFile('./pets.json', (err, data) => {
     if (err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(err)
+      err = true;
       console.error(err);
-      process.exit(1);
     } else {
       data = JSON.parse(data);
       if (index) {
         if (index < data.length && index >= 0) {
+          res.writeHead(200, {'Content-Type': 'application/json'});
           res.write(JSON.stringify(data[index]));
         } else {
+          res.writeHead(400, {'Content-Type': 'text/plain'});
+          res.end('Invalid index');
+          err = true;
           console.error('Usage: node pets.js read INDEX');
-          process.exit(1);
         }
       } else {
+        res.writeHead(200, {'Content-Type': 'application/json'});
         res.write(JSON.stringify(data));
       }
     }
     readCounts--;
-    if (readCounts <=0) {
+    if (readCounts <= 0) {
       res.end();
     }
   });
-}
-
-function error() {
-
 }
 
 server.listen(3000);
