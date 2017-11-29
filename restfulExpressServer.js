a7b03b41f93b847b5a9c4656fb9b35ae9352bb8c
Jason Amador <jason.amador@gmail.com>
2017-11-28 18:38:40 -0600

    should mostly work, test needs to reset the file

---
+++ restfulExpressServer.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+
+app.get('/pets', (req, res) => {
+  fs.readFile('./pets.json', (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      try {
+        res.send(JSON.parse(data));
+      } catch (e){
+        res.sendStatus(500);
+      }
+    }
+  });
+});
+
+app.get('/pets/:id', (req, res) => {
+  fs.readFile('./pets.json', (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      let id = Number.parseInt(req.params.id);
+      let pets;
+      try {
+        pets = JSON.parse(data);
+      } catch (e) {
+        res.sendStatus(500);
+      }
+      if (id >= 0 && id < pets.length) {
+        res.send(pets[id]);
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  });
+});
+
+app.post('/pets', (req, res) => {
+  fs.readFile('./pets.json', (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      let pets;
+      try {
+        pets = JSON.parse(data);
+      } catch (e) {
+        res.sendStatus(500);
+      }
+      const pet = req.body;
+      if (!isNaN(pet.age) && pet.name && pet.kind) {
+        pets.push(pet);
+        fs.writeFile('./pets.json', JSON.stringify(pets), (err) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            res.send(req.body);
+          }
+        });
+      } else {
+        res.sendStatus(400);
+      }
+    }
+  });
+});
+
+app.delete('/pets/:id', (req, res) => {
+  fs.readFile('./pets.json', (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      const id = Number.parseInt(req.params.id);
+      let pets;
+      try {
+        pets = JSON.parse(data);
+      } catch(e) {
+        res.sendStatus(500);
+      }
+      if (id >= 0 && id < pets.length) {
+        let pet = pets.splice(id, 1)[0];
+        fs.writeFile('./pets.json', JSON.stringify(pets), (err) => {
+          if (err) {
+            res.sendStatus(500);
+          } else {
+            res.send(pet);
+          }
+        });
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  });
+});
+
+app.patch('/pets/:id', (req, res) => {
+  fs.readFile('./pets.json', (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      const id = Number.parseInt(req.params.id);
+      const patch = req.body;
+      let pets;
+      try {
+        pets = JSON.parse(data);
+      } catch (e) {
+        res.sendStatus(500);
+      }
+      const pet = pets[id];
+      for (let key in patch) {
+        switch (key) {
+          case 'name':
+            pet.name = patch.name;
+            break;
+          case 'kind':
+            pet.kind = patch.kind;
+            break;
+          case 'age':
+            if (!isNaN(Number.parseInt(patch.age))) {
+              pet.age = patch.age;
+            } else {
+              res.sendStatus(400);
+            }
+            break;
+          default: res.sendStatus(400);
+        }
+      }
+      pets[id] = pet;
+      fs.writeFile('./pets.json', JSON.stringify(pets), (err) => {
+        if (err) {
+          res.sendStatus(500);
+        } else {
+          res.send(pet);
+        }
+      })
+    }
+  });
+});
+
+
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
+
+module.exports = app;
