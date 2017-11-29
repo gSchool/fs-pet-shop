4692e0ee9a45bd93b5b6832abfb883379d42be64
Lance Hunter <lancehunter@gmail.com>
2017-11-29 10:01:14 -0600

    Completed base assignment for FS section.

--- pets.js
+++ pets.js
@@ -22,8 +22,40 @@ switch (process.argv[2].toUpperCase()) {
           process.exit(1);
         }
       }
-    })
+    });
     break;
 
   case "CREATE" :
+    fs.readFile('./pets.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      petArr = JSON.parse(data);
+      if ((!process.argv[3]) || (!process.argv[4]) || (!process.argv[5])) {
+        console.error('Usage: node pets.js create AGE KIND NAME');
+        process.exit(1);
+      } else {
+        let petObj = {};
+        petObj.age = parseInt(process.argv[3]);
+        petObj.kind = process.argv[4];
+        petObj.name = process.argv[5]
+        petArr.push(petObj);
+        output = JSON.stringify(petArr);
+        fs.writeFile('./pets.json', output, (err) => {
+          if (err) throw err;
+          console.log(petObj);
+        });
+      }
+    });
+    break;
+
+    case "UPDATE" :
+      fs.readFile('./pets.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        petArr = JSON.parse(data);
+        if ((!process.argv[3]) || (!process.argv[4]) || (!process.argv[5]) || (!process.argv[6])) {
+          console.error('Usage: node pets.js update INDEX AGE KIND NAME');
+          process.exit(1);
+        }
+      });
+      break;
+
 }
