f40c1790f2a108e0a52ea28ce439d82cbbd03279
Lance Hunter <lancehunter@gmail.com>
2017-11-30 15:26:28 -0600

    Completed Express basic tests.

--- expressServer.js
+++ expressServer.js
@@ -12,8 +12,33 @@ app.get('/pets', (req, res) => {
     if (err) throw err;
     petArr = JSON.parse(data);
     res.type('application/json');
-    res.send(JSON.stringify(petArr), );
+    res.send(JSON.stringify(petArr));
   });
+});
+
+
+app.get('/pets/:id', (req, res) => {
+  var filterInt = function(value) {
+  if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
+    return Number(value);
+  return NaN;
+  }
+  let indexSpot = filterInt(req.params.id);
+  console.log(isNaN(indexSpot));
+  if (!isNaN(indexSpot)) {
+    fs.readFile('./pets.json', 'utf8', (err, data) => {
+      if (err) {res.sendStatus(500)};
+      petArr = JSON.parse(data);
+      if (indexSpot<0 || indexSpot>=petArr.length) {
+        res.sendStatus(404);
+      } else {
+        petObj = petArr[indexSpot];
+        console.log(petObj);
+//        res.type('application/json');
+        res.send(petObj);
+      }
+    });
+  }
 });
 
 app.use((req, res) => {
