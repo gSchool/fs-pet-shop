89e3c574ab68439ac8779342f2caa2c34ed274d8
Jason Amador <jason.amador@gmail.com>
2017-11-29 18:15:37 -0600

    merged test fixes with the original repo

--- test/expressServer.test.js
+++ test/expressServer.test.js
@@ -2,39 +2,83 @@
 
 const rewire = require('rewire');
 const request = require('supertest');
+const fs = require('fs');
 
 // Set the port to a different number so that it does not conflict with the
 // other test files.
 process.env.PORT = 3003;
 const app = rewire('../expressServer');
 
-describe('pets expressServer', () => {
-  beforeEach(() => {
-    let petsArr = [{
-      age: 7,
-      kind: 'rainbow',
-      name: 'fido'
-    }, {
-      age: 5,
-      kind: 'snake',
-      name: 'Buttons'
-    }];
+const copyFile = function(source, target, cb) {
+  let cbCalled = false;
+  const rd = fs.createReadStream(source);
+  const wr = fs.createWriteStream(target);
+  const done = function(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  };
 
-    app.__set__({
-      'fs': {
-        readFile: function(path, encoding, cb){
-          if(/pets.json$/.test(path)) return cb(null,JSON.stringify(petsArr));
-          cb(new Error('File does not exist'));
-        },
-        writeFile: function(path, data, cb){
-          if(/pets.json$/.test(path)){
-            petsArr = JSON.parse(data);
-            return cb(null);
-          }
-          return cb(new Error('File does not exist'));
-        }
+  rd.on('error', (err) => {
+    done(err);
+  });
+
+  wr.on('error', (err) => {
+    done(err);
+  });
+
+  wr.on('close', (_ex) => {
+    done();
+  });
+
+  rd.pipe(wr);
+};
+
+describe('pets expressServer', () => {
+  beforeEach((done) => {
+    copyFile('pets.json', 'pets.json.old', (err) => {
+      if (err) {
+        return done(err);
       }
-    })
+
+      const petsArr = [{
+        age: 7,
+        kind: 'rainbow',
+        name: 'fido'
+      }, {
+        age: 5,
+        kind: 'snake',
+        name: 'Buttons'
+      }];
+
+      fs.writeFile('pets.json', JSON.stringify(petsArr), done);
+      app.__set__({
+        'fs': {
+          readFile: function(path, encoding, cb){
+            if(/pets.json$/.test(path)) return cb(null,JSON.stringify(petsArr));
+            cb(new Error('File does not exist'));
+          },
+          writeFile: function(path, data, cb){
+            if(/pets.json$/.test(path)){
+              petsArr = JSON.parse(data);
+              return cb(null);
+            }
+            return cb(new Error('File does not exist'));
+          }
+        }
+      });
+    });
+  });
+
+  afterEach((done) => {
+    copyFile('pets.json.old', 'pets.json', (err) => {
+      if (err) {
+        return done(err);
+      }
+
+      fs.unlink('pets.json.old', done);
+    });
   });
 
 
