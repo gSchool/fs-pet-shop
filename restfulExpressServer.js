686b22d786256b09dd1708bfc1d304895e36dadc
Lance Hunter <lancehunter@gmail.com>
2017-12-01 12:06:26 -0600

    Completed bonus sections for RESTful express server.

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -7,6 +7,16 @@ let bodyParser = require('body-parser');
 let petsRoute = require('./routes/pets.js');
 let port = process.env.PORT || 8888;
 
+app.use((req, res, next) => {
+  console.log(req.headers);
+  if (req.headers.authorization === 'Basic YWRtaW46bWVvd21peA==') {
+    next();
+  } else {
+    res.set('WWW-Authenticate', 'Basic realm="Required"');
+    res.sendStatus(401);
+  }
+});
+
 app.use(bodyParser.json());
 
 app.use('/pets', petsRoute);
