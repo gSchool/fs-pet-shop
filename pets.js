4b687db325ca55934cfd76242892e2191eabd7b5
David Nelson Murray <linearsix@gmail.com>
2018-09-05 11:41:43 -0500

    all tests passing including bonuses

--- pets.js
+++ pets.js
@@ -1,37 +1,42 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 
-if (process.argv[2] === 'read') {
-    fs.readFile('./pets.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Usage: node pets.js [read | create | update | destroy]`);
-            process.exitCode = 1;    
-        } else {
-            if (process.argv[3]) {
-                let recordNumber = Number(process.argv[3]);
-                let recordData = JSON.parse(data);
-                let recordSelect = recordData[recordNumber];
-                if (recordSelect === undefined) {
-                    console.error('Usage: node pets.js read INDEX')
-                    process.exitCode = 1;
-                } else {
-                    console.log(recordSelect);
-                };
+// READ THE .JSON FILE
+fs.readFile('./pets.json', 'utf8', (err, data) => {
+    const recordData = JSON.parse(data);
+    // error handling
+    if (err) {
+        console.error(`Usage: node pets.js [read | create | update | destroy]`);
+        process.exitCode = 1;    
+    };
+    
+    // IF 'READ'
+    if (process.argv[2] === 'read') {
+        // if there is an index specified, get that array element
+        if (process.argv[3]) {
+            let recordNumber = Number(process.argv[3]);
+            let recordSelect = recordData[recordNumber];
+            // handle error if index out of range
+            if (recordSelect === undefined) {
+                console.error('Usage: node pets.js read INDEX')
+                process.exitCode = 1;
             } else {
-                let recordData = JSON.parse(data);
-                console.log(recordData);
+                // show the data
+                console.log(recordSelect);
             };
+        } else {
+            // no index specified so show all data
+            console.log(recordData);
         };
-    });
-} else if (process.argv[2] === 'create') {
-    fs.readFile('./pets.json', 'utf8', (err, data) => {
-        
-        let recordData = JSON.parse(data);
+    // CREATE A NEW RECORD
+    } else if (process.argv[2] === 'create') {
+            
+        // assign argv items to variables
         let recordAge = Math.floor(Number(process.argv[3]));
-        // console.log(recordAge);
         let recordKind = process.argv[4];
-        // console.log(recordKind);
         let recordName = process.argv[5];
-        // console.log(recordName);
+        // if all arguments are there, write the new record
         if (recordAge && recordKind && recordName) {
             let recordNew = new Object();
             recordNew.age = recordAge;
@@ -48,38 +53,47 @@ if (process.argv[2] === 'read') {
             console.error('Usage: node pets.js create AGE KIND NAME');
             process.exitCode = 1;
         };
-    });
-} else if (process.argv[2] === 'destroy') {
-    fs.readFile('./pets.json', 'utf8', (err, data) => {
-        
-        let recordData = JSON.parse(data);
-        let recordDestroy = Math.floor(Number(process.argv[3]));
-        console.log(recordDestroy);
-        if (recordDestroy) {
-            recordData.splice(recordDestroy, 1);
-            fs.writeFile('pets.json', JSON.stringify(recordData), (err) => {
-                if (err) {
+
+    // DESTROY RECORD
+    } else if (process.argv[2] === 'destroy') {
+
+        if (process.argv[3]) {
+            let recordNumber = Number(process.argv[3]);
+            let recordSelect = recordData[recordNumber];
+            // handle error if index out of range
+            if (recordSelect === undefined) {
+                console.error('Usage: node pets.js destroy INDEX')
+                process.exitCode = 1;
+            } else {
+                // show the data
+                console.log(recordSelect);
+                let recordDestroy = Math.floor(Number(process.argv[3]));
+                // if the index is there, remove the record
+                if (recordDestroy) {
+                    recordData.splice(recordDestroy, 1);
+                    fs.writeFile('pets.json', JSON.stringify(recordData), (err) => {
+                        if (err) {
+                            console.error('Usage: node pets.js destroy INDEX');
+                            process.exitCode = 1;
+                        };
+                    });
+                } else {
                     console.error('Usage: node pets.js destroy INDEX');
                     process.exitCode = 1;
                 };
-            });
+            };
         } else {
             console.error('Usage: node pets.js destroy INDEX');
             process.exitCode = 1;
-        };
-    });
-} else if (process.argv[2] === 'update') {
-    fs.readFile('./pets.json', 'utf8', (err, data) => {
-        
-        let recordData = JSON.parse(data);
+        }; 
+    } else if (process.argv[2] === 'update') {
+            
+        // assign argv to variables
         let recordNumber = Math.floor(Number(process.argv[3]));
-        console.log(recordNumber);
         let recordAge = Math.floor(Number(process.argv[4]));
-        console.log(recordAge);
         let recordKind = process.argv[5];
-        console.log(recordKind);
         let recordName = process.argv[6];
-        console.log(recordName);
+        // if all argv variables are accounted for
         if (recordNumber && recordAge && recordKind && recordName) {
             let recordNew = new Object();
             recordNew.age = recordAge;
@@ -94,8 +108,11 @@ if (process.argv[2] === 'read') {
                 console.log(recordNew);
             });
         } else {
-            console.error('Usage: node pets.js updtae AGE KIND NAME');
+            console.error('Usage: node pets.js update INDEX AGE KIND NAME');
             process.exitCode = 1;
         };
-    });
-};
+    } else {
+        console.error(`Usage: node pets.js [read | create | update | destroy]`);
+        process.exitCode = 1;
+    };
+});
