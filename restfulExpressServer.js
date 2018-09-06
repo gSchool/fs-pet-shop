34c6edbd3dad3c8be5516d8c1ce7306297d17300
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-09-06 16:08:34 -0500

    able to get with index

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -14,10 +14,27 @@ app.get('/pets', function(req,res) {
       console.error(err.stack);
       return res.sendStatus(500);
     }
+
     let pets = JSON.parse(petsJSON);
     res.send(pets);
   })
 })
+app.get('/pets/:id', function(req,res) {
+  fs.readFile(petsPath, 'utf8', function ( err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sentStatus(500);
+    }
+  let id = Number.parseInt(req.params.id);
+  let pets = JSON.parse(petsJSON);
+
+  if (id < 0 || id>= pets.length || Number.isNaN(id)) {
+    return res.sendStatus (404)
+  }
+  res.set('Content-Type', 'text/plain');
+  res.send (pets[id]);
+  });
+});
 
 app.use( function (req, res) {
   res.sendStatus(404)
