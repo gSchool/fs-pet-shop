f06921f62facedcfe34e4f4161cc67a89ce268c1
Abellarr <da.ortiz83@gmail.com>
2023-02-16 13:21:46 -0800

    Updated with httpServer.js to handle requirements up to the bonuses

---
+++ httpServer.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const http = require('http');
+
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    const URL = req.url
+    console.log(URL);
+    const method = req.method
+    console.log(method);
+    let urlArray = URL.split('/');
+    let urlPetNumber = urlArray[2];
+    let urlPetInt = parseInt(urlPetNumber);
+    fs.readFile('pets.json', 'utf8', function(err, data){
+        let allPets = JSON.parse(data);
+        let allPetsJSON = JSON.stringify(allPets);
+        if (err){
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain')
+            res.end('Internal server error');
+            return;
+        } else if (urlPetInt) {
+            singlePet(urlPetInt);
+        } else if (URL == '/pets') {
+            getAll();
+        }
+        
+        function getAll() {
+            res.setHeader('Content-Type', 'application/json');
+            res.statusCode = 200;
+            res.end(allPetsJSON);
+            return;
+        }
+        
+        function singlePet(num) {
+            if (!allPets[urlPetInt]){
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain')
+            res.end('Not Found');
+            return;
+            } else {
+                res.setHeader('Content-Type', 'application/json');
+                res.statusCode = 200;
+                res.end(JSON.stringify(allPets[urlPetInt]));
+                return;
+            }
+        }
+    })
+})
+
+server.listen(port, ()=> {
+    console.log(`Listening on port ${port}`);
+})
