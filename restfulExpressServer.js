8046d5aba7752457a5c65335b872ac9091dd4b9e
Abellarr <da.ortiz83@gmail.com>
2023-02-28 13:15:18 -0800

    Created restful server file and populated GET and POST requests

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const port = 8000;
 app.use(express.json());
 
+// If GET request to /pets, returns all info to client
 app.get('/pets', (req,res,next)=>{
     console.log(req.method);
     console.log('Request for All Pets')
@@ -12,10 +13,12 @@ app.get('/pets', (req,res,next)=>{
             next(err);
         }
         const allPets=JSON.parse(data);
+        res.status(200);
         res.send(allPets);
     })
 })
 
+// If GET request to specific pet id at that path, returns info about that pet
 app.get('/pets/:id/', (req,res,next)=>{
     console.log(req.method);
     const id = parseInt(req.params.id);
@@ -25,25 +28,39 @@ app.get('/pets/:id/', (req,res,next)=>{
         if (!allPets[id]) {
             next({});
         } else {
+            res.status(200);
             res.send(allPets[id]);
         }
     })
 })
 
-app.post('/pets', (req,res)=>{
+app.use((err,req,res,next)=>{
+    console.log('GET error sent to middleware')
+    console.error(err.stack);
+    res.status(404).send('Not Found');
+})
+
+// If POST request, takes the request in and adds the information to the pets.json file
+app.post('/pets', (req,res, next)=>{
     console.log(req.method);
-    let newPet = req.body;
-    console.log(newPet);
+    const age = parseInt(req.body.age);
+    const { kind, name } = req.body;
+    console.log(`${age}, ${kind}, ${name}`);
+    if (!age || !kind || !name || Number.isNaN(age)){
+        next(err);
+    }
     fs.readFile('./pets.json', (err,data)=>{
         if (err){
             next(err);
         } else {
             let allPets = JSON.parse(data);
+            let newPet = {age, kind, name};
             allPets.push(newPet);
             fs.writeFile('./pets.json', JSON.stringify(allPets), (err)=>{
                 if (err) {
                     next(err);
                 } else {
+                    res.status(200)
                     res.send(`New pet added: ${JSON.stringify(newPet)}`);
                 }
             })
@@ -51,14 +68,11 @@ app.post('/pets', (req,res)=>{
     })   
 })
 
-app.get('/test/', (req,res,next)=>{
-    res.send('Hello, my friend! You shouldn\'t be here, this is for testing and development. Now, shoo you, get out of here. Go back to looking at our pets!')
-})
-
 app.use((err,req,res,next)=>{
-    console.log('Error sent to middleware')
+    console.log('POST error sent to middleware')
     console.error(err.stack);
-    res.status(404).send('Not Found');
+    res.status(500).send('Internal Error');
+})
 
 
 app.listen(port, ()=>{
