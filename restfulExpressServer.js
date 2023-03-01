e26414d878f34da17ec83b8d492a0bd4a16345f0
Abellarr <da.ortiz83@gmail.com>
2023-03-01 13:27:24 -0800

    Updated to add DELETE request. Tested and working. Still working on PATCH request.

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -27,7 +27,7 @@ app.get('/pets', (req,res,next)=>{
         let rows = result.rows;
         res.status(200).send(rows);
     })
-    // fs.readFile(petsPath, (err,data)=>{                  Old code from when the DB was in a JSON file
+    // fs.readFile(petsPath, (err,data)=>{                  ***Code from when the DB was in a JSON file***
     //     if (err){
     //         next(err);
     //     }
@@ -56,7 +56,7 @@ app.get('/pets/:id/', (req,res,next)=>{
         }
     })
 
-    // fs.readFile(petsPath, (err,data)=>{                  Old code from when the DB was in a JSON file
+    // fs.readFile(petsPath, (err,data)=>{                  ***Code from when the DB was in a JSON file***
     //     const allPets=JSON.parse(data);
     //     if (!allPets[id]) {
     //         console.log('DELETE error, doesn\'t exist');
@@ -68,13 +68,14 @@ app.get('/pets/:id/', (req,res,next)=>{
     // })
 })
 
-// app.use((err,req,res,next)=>{
+// app.use((err,req,res,next)=>{                            ***Code from when the DB was in a JSON file***
 //     console.log('GET error sent to middleware')
 //     console.error(err.stack);
 //     res.status(404).send('Not Found');
 // })
 
-// If POST request, takes the request in and adds the information to the pets.json file
+
+// If POST request to /pets, takes the request in and adds the information to the pets.json file
 app.post('/pets', (req,res, next)=>{
     console.log(req.method);
     const age = parseInt(req.body.age);
@@ -84,13 +85,16 @@ app.post('/pets', (req,res, next)=>{
         return res.status(400).send('Error: Input missing information');
     } else {
         pool.query('INSERT INTO pets (name, kind, age) VALUES ($1, $2, $3) RETURNING *;', [name, kind, age], (err, result)=>{
+            if (err){
+                return next(err);
+            }
             let petInfo = result.rows;
             console.log('Added: ' + petInfo);
             res.status(200).send(petInfo);
         })
     }
 
-    // fs.readFile(petsPath, (err,data)=>{
+    // fs.readFile(petsPath, (err,data)=>{                  ***Code from when the DB was in a JSON file***
     //     if (err){
     //         next(err);
     //     } else {
@@ -113,72 +117,109 @@ app.post('/pets', (req,res, next)=>{
     // })   
 })
 
-app.use((err,req,res,next)=>{
-    console.log('POST error sent to middleware')
-    return res.status(500).send('Internal Error');
+// app.use((err,req,res,next)=>{                            ***Code from when the DB was in a JSON file***
+//     console.log('POST error sent to middleware')
+//     return res.status(500).send('Internal Error');
+// })
+
+// If PATCH request to pets/#, takes in the body and checks against
+app.patch('/pets/:id', (req,res, next)=>{
+    console.log(req.method);
+    const id = parseInt(req.params.id);
+    const { name, kind, age } = req.body;
+    let petAge = parseInt(age);
+    if (name){
+        pool.query('UPDATE pets SET name = $1 WHERE id = $2;', [name, id], (err, result)=>{
+            console.log('Pet updated');
+        });
+    };
+    if (kind){
+        pool.query('UPDATE pets SET kind = $1 WHERE id = $2;', [kind, id], (err, result)=>{
+
+        });
+    };
+    if (age && !Number.isNaN(petAge)){
+        pool.query('UPDATE pets SET age = $1 WHERE id = $2;', [age, id], (err, result)=>{
+
+        });
+    };
+    
+
+    // fs.readFile(petsPath, (err,data)=>{                  ***Code from when the DB was in a JSON file***    
+    //     let allPets = JSON.parse(data);
+    //     if (err || !allPets[petId] || Number.isNaN(parseInt(body.age))) {
+    //         console.log('PATCH error')
+    //         return res.status(400).send('Improper input from user');
+    //     }
+    //     if (body.hasOwnProperty('age' && !Number.isNaN(parseInt(body.age)))) {
+    //         allPets[petId].age = body.age;
+    //     }
+    //     if (body.hasOwnProperty('kind')) {
+    //         allPets[petId].kind = body.kind;
+    //     }
+    //     if (body.hasOwnProperty('name')) {
+    //         allPets[petId].name = body.name;
+    //     }
+    //     fs.writeFile(petsPath, JSON.stringify(allPets), (err)=>{
+    //         if (err) {
+    //             next(err);
+    //         } else {
+    //             res.status(200)
+    //             res.send(`Updated pet info`);
+    //         }
+    //      })        
+    // })   
 })
 
-// If PATCH request, takes in the body and checks against
-app.patch('/pets/:pet_id', (req,res, next)=>{
+// app.use((err,req,res,next)=>{                            ***Code from when the DB was in a JSON file***
+//     console.log('PATCH error sent to middleware')
+//     console.error(err.stack);
+//     res.status(500).send('Internal Error');
+// })
+
+app.delete('/pets/:id', (req, res, next)=>{
     console.log(req.method);
-    const petId = parseInt(req.params.pet_id);
-    const body = req.body;
-    fs.readFile(petsPath, (err,data)=>{
-        let allPets = JSON.parse(data);
-        if (err || !allPets[petId] || Number.isNaN(parseInt(body.age))) {
-            console.log('PATCH error')
-            return res.status(400).send('Improper input from user');
-        }
-        if (body.hasOwnProperty('age' && !Number.isNaN(parseInt(body.age)))) {
-            allPets[petId].age = body.age;
-        }
-        if (body.hasOwnProperty('kind')) {
-            allPets[petId].kind = body.kind;
-        }
-        if (body.hasOwnProperty('name')) {
-            allPets[petId].name = body.name;
-        }
-        fs.writeFile(petsPath, JSON.stringify(allPets), (err)=>{
-            if (err) {
-                next(err);
+    const id = parseInt(req.params.id);
+    if (Number.isNaN(id)) {
+        console.log('Error Not Found');
+        return res.status(404).send('Error Not Found');
+    } else {
+        pool.query('DELETE FROM pets WHERE id = $1 RETURNING *;', [id], (err, result)=>{
+            if (err){
+                return next(err);
+            }
+            let delPet = result.rows[0];
+            if (delPet){
+                console.log(delPet);
+                res.status(200).send(delPet);
             } else {
-                res.status(200)
-                res.send(`Updated pet info`);
+                console.log('Pet not found');
+                res.status(404).send('Pet not found');
             }
-         })        
-    })   
-})
+        })
+    }
 
-app.use((err,req,res,next)=>{
-    console.log('PATCH error sent to middleware')
-    console.error(err.stack);
-    res.status(500).send('Internal Error');
-})
-
-app.delete('/pets/:pet_id', (req, res, next)=>{
-    console.log(req.method);
-    const petId = parseInt(req.params.pet_id);
-    fs.readFile(petsPath, (err,data)=>{
-        if (err){
-            return next(err);
-        } else {
-            let allPets = JSON.parse(data);
-            if (!allPets[petId]) {
-                console.log('DELETE error, doesn\'t exist');
-                return res.status(404).send('Not Found');
-            }
-            // let deletedPet = allPets[petId];
-            allPets.splice(petId, 1);
-            fs.writeFile(petsPath, JSON.stringify(allPets), (err)=>{
-                if (err) {
-                    return next(err);
-                } else {
-                    res.status(200)
-                    res.send(`Pet deleted`);
-                }
-            })
-        }        
-    })
+    // fs.readFile(petsPath, (err,data)=>{                  ***Code from when the DB was in a JSON file***
+    //     if (err){
+    //         return next(err);
+    //     } else {
+    //         let allPets = JSON.parse(data);
+    //         if (!allPets[petId]) {
+    //             console.log('DELETE error, doesn\'t exist');
+    //             return res.status(404).send('Not Found');
+    //         }
+    //         // let deletedPet = allPets[petId];
+    //         allPets.splice(petId, 1);
+    //         fs.writeFile(petsPath, JSON.stringify(allPets), (err)=>{
+    //             if (err) {
+    //                 return next(err);
+    //             } else {
+    //                 res.status(200)
+    //                 res.send(`Pet deleted`);
+    //             }
+    //         })
+    //     }        
+    // })
 })
 
 app.use((err,req,res,next)=>{
