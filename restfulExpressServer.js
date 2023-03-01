e80a142aaf8ca623fe98354887a06a935b4ad408
Abellarr <da.ortiz83@gmail.com>
2023-03-01 12:54:23 -0800

    Reconfigured GET and POST requests, and dependencies to work with PostgreSQL. Tested and working.

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -1,86 +1,116 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const path = require('path');
-const petsPath = path.join(__dirname, 'pets.json');
+// const path = require('path');
+// const petsPath = path.join(__dirname, 'pets.json');
+const { Pool } = require('pg')
 const port = 8000;
 app.use(express.json());
 
-app.all('/', (req,res,next)=>{
-    return next(err);
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'petshop',
+    password: 'mcsp19',
+    port: 5432,
 })
-// app.all('/:any/', (req,res,next)=>{
-//     let wrongPath = req.params.any;
-//     if (any !== 'pets') {
-//         console.log('Error, incorrect path');
-//         res.status(404).send('Not Found');
-//     }
-// })
+
 
 // If GET request to /pets, returns all info to client
 app.get('/pets', (req,res,next)=>{
     console.log(req.method);
     console.log('Request for All Pets')
-    fs.readFile(petsPath, (err,data)=>{
+    pool.query('SELECT * FROM pets;', (err, result)=>{
         if (err){
-            next(err);
+            return next(err);
         }
-        const allPets=JSON.parse(data);
-        res.status(200);
-        res.send(allPets);
+        let rows = result.rows;
+        res.status(200).send(rows);
     })
+    // fs.readFile(petsPath, (err,data)=>{                  Old code from when the DB was in a JSON file
+    //     if (err){
+    //         next(err);
+    //     }
+    //     const allPets=JSON.parse(data);
+    //     res.status(200);
+    //     res.send(allPets);
+    // })
 })
 
 // If GET request to specific pet id at that path, returns info about that pet
 app.get('/pets/:id/', (req,res,next)=>{
     console.log(req.method);
     const id = parseInt(req.params.id);
-    console.log(`Request for pets at /${id}`)
-    fs.readFile(petsPath, (err,data)=>{
-        const allPets=JSON.parse(data);
-        if (!allPets[id]) {
-            console.log('DELETE error, doesn\'t exist');
-            return res.status(404).send('Not Found');
+    console.log(`Request for pets at /${id}`);
+    pool.query('SELECT * FROM pets WHERE id = $1;', [id], (err, result)=>{
+        if (err){
+            return next(err);
+        }
+        let pet = result.rows[0];
+        if (pet){
+            console.log(pet);
+            res.status(200).send(pet);
         } else {
-            res.status(200);
-            res.send(allPets[id]);
+            console.log('Pet not found');
+            res.status(404).send('Pet not found');
         }
     })
+
+    // fs.readFile(petsPath, (err,data)=>{                  Old code from when the DB was in a JSON file
+    //     const allPets=JSON.parse(data);
+    //     if (!allPets[id]) {
+    //         console.log('DELETE error, doesn\'t exist');
+    //         return res.status(404).send('Not Found');
+    //     } else {
+    //         res.status(200);
+    //         res.send(allPets[id]);
+    //     }
+    // })
 })
 
-app.use((err,req,res,next)=>{
-    console.log('GET error sent to middleware')
-    console.error(err.stack);
-    res.status(404).send('Not Found');
-})
+// app.use((err,req,res,next)=>{
+//     console.log('GET error sent to middleware')
+//     console.error(err.stack);
+//     res.status(404).send('Not Found');
+// })
 
 // If POST request, takes the request in and adds the information to the pets.json file
 app.post('/pets', (req,res, next)=>{
     console.log(req.method);
     const age = parseInt(req.body.age);
     const { kind, name } = req.body;
-    console.log(`${age}, ${kind}, ${name}`);
-    fs.readFile(petsPath, (err,data)=>{
-        if (err){
-            next(err);
-        } else {
-            if (!age || !kind || !name || Number.isNaN(age)) {
-                console.log('Error: Input missing information');
-                return res.status(400).send('Error: Input missing information');
-            }
-            let allPets = JSON.parse(data);
-            let newPet = {age, kind, name};
-            allPets.push(newPet);
-            fs.writeFile(petsPath, JSON.stringify(allPets), (err)=>{
-                if (err) {
-                    next(err);
-                } else {
-                    res.status(200)
-                    res.send(`New pet added: ${JSON.stringify(newPet)}`);
-                }
-            })
-        }        
-    })   
+    if (!age || !kind || !name || Number.isNaN(age)) {
+        console.log('Error: Input missing information');
+        return res.status(400).send('Error: Input missing information');
+    } else {
+        pool.query('INSERT INTO pets (name, kind, age) VALUES ($1, $2, $3) RETURNING *;', [name, kind, age], (err, result)=>{
+            let petInfo = result.rows;
+            console.log('Added: ' + petInfo);
+            res.status(200).send(petInfo);
+        })
+    }
+
+    // fs.readFile(petsPath, (err,data)=>{
+    //     if (err){
+    //         next(err);
+    //     } else {
+    //         if (!age || !kind || !name || Number.isNaN(age)) {
+    //              console.log('Error: Input missing information');
+    //              return res.status(400).send('Error: Input missing information');
+    //         }
+    //         let allPets = JSON.parse(data);
+    //         let newPet = {age, kind, name};
+    //         allPets.push(newPet);
+    //         fs.writeFile(petsPath, JSON.stringify(allPets), (err)=>{
+    //             if (err) {
+    //                 next(err);
+    //             } else {
+    //                 res.status(200)
+    //                 res.send(`New pet added: ${JSON.stringify(newPet)}`);
+    //             }
+    //         })
+    //     }        
+    // })   
 })
 
 app.use((err,req,res,next)=>{
